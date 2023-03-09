824750e13bf3ff35f689a9c486744d787a97aca9
Jinsuo <cil04110@gmail.com>
2023-03-09 11:02:35 +0900

    #1 - init project init project using spring boot initializer, add basic libraries and gitignore

---
+++ src/main/java/com/project/pinkwhite/PinkwhiteApplication.java
@@ -0,0 +1,13 @@
+package com.project.pinkwhite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinkwhiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PinkwhiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/pinkwhite/PinkwhiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.pinkwhite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinkwhiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
