5897b8f25d3ac87c352f34f8686d21cb0c2ebfea
Jinsuo <cil04110@gmail.com>
2023-03-09 12:41:22 +0900

    #2 - jpa auditing
    jpa entity auditing setting

---
+++ src/main/java/com/project/pinkwhite/config/AuditorConfig.java
@@ -0,0 +1,17 @@
+package com.project.pinkwhite.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditorConfig {
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return () -> Optional.of("jinsuo");
+    }
+}
