9a7d6ce2c7ef25b9e2fe756388ab544437bd804b
Jinsuo <cil04110@gmail.com>
2023-03-09 12:42:55 +0900

    #2 - article, article comment, member entity
    make entity and tables to database

---
+++ src/main/java/com/project/pinkwhite/domain/Article.java
@@ -0,0 +1,67 @@
+package com.project.pinkwhite.domain;
+
+import com.project.pinkwhite.domain.base.BaseEntity;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Getter
+@ToString(callSuper = true)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(indexes = {
+                @Index(columnList = "title"),
+                @Index(columnList = "hashtag"),
+                @Index(columnList = "createdAt"),
+                @Index(columnList = "createdBy"),
+})
+@Entity
+public class Article extends BaseEntity {
+    @Id @GeneratedValue
+    @Column(name = "article_id")
+    private Long id;
+
+    @Setter
+    @Column(nullable = false) private String title;
+    @Setter
+    @Column(nullable = false, length = 10000)
+    private String content;
+    private String hashtag;
+
+    @ToString.Exclude
+    @OrderBy(value = "createdAt DESC")
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
+    private Set<ArticleComment> articleComments = new LinkedHashSet<>();
+
+    @Setter
+    @ToString.Exclude
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private Article(String title, String content, String hashtag, Member member) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+        this.member = member;
+    }
+
+    public static Article of(String title, String content, String hashtag, Member member) {
+        return new Article(title, content, hashtag, member);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article)) return false;
+        Article article = (Article) o;
+        return id != null && getId().equals(article.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
---
+++ src/main/java/com/project/pinkwhite/domain/ArticleComment.java
@@ -0,0 +1,63 @@
+package com.project.pinkwhite.domain;
+
+import com.project.pinkwhite.domain.base.BaseEntity;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Getter
+@ToString(callSuper = true)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(indexes = {
+        @Index(columnList = "content"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy"),
+})
+@Entity
+public class ArticleComment extends BaseEntity {
+    @Id @GeneratedValue
+    @Column(name = "article_comment_id")
+    private Long id;
+
+    @Setter
+    private String content;
+
+    @Setter
+    @ToString.Exclude
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "article_article_id")
+    private Article article;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    @ToString.Exclude
+    private Member member;
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    private ArticleComment(Long id, String content, Article article, Member member) {
+        this.id = id;
+        this.content = content;
+        this.article = article;
+        this.member = member;
+    }
+    public static ArticleComment of(Long id, String content, Article article, Member member) {
+        return new ArticleComment(id, content, article, member);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ArticleComment)) return false;
+        ArticleComment that = (ArticleComment) o;
+        return id != null && getId().equals(that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
